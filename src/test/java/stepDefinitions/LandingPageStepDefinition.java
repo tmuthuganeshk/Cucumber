659df38b0ce7a296db1869b1b021c35912737611
tmuthuganeshk <tmuthuganesh2000@gmail.com>
2025-11-04 20:50:51 +0530

    Second commit

--- src/test/java/stepDefinitions/LandingPageStepDefinition.java
+++ src/test/java/stepDefinitions/LandingPageStepDefinition.java
@@ -30,7 +30,7 @@ public LandingPageStepDefinition(TestContextSetup testContextSetup)
 }
 	@Given("User is on GreenCart Landing page")
 public void user_is_on_green_cart_landing_page() {
-		
+		System.out.println("hiii");
 		Assert.assertTrue(landingPage.getTitleLandingPage().contains("GreenKart"));
 }
 
